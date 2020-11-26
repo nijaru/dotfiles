4a76f90fae78d3bb31ab37bf6cf4fbf138f0a999
Nick Russo <nijaru0x@gmail.com>
2020-11-26 13:01:50 -0800

    script: add install and backup scripts
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+cp -f $HOME/.aliases .
+cp -f $HOME/.p10k.zsh .
+cp -f $HOME/.zpreztorc .
+cp -f $HOME/.zprofile .
+cp -f $HOME/.zshrc .
+cp -f $HOME/.vim/vimrc .
+
+echo "Backup complete."
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+cp -nf .aliases $HOME
+cp -nf .p10k.zsh $HOME
+cp -nf .zpreztorc $HOME
+cp -nf .zprofile $HOME
+cp -nf .zshrc $HOME
+cp -nf vimrc $HOME/.vim
+
+echo "Installation to $HOME complete."

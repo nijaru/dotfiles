3ffaba8d4d20062b7afb61cb60a28017d9a4853a
Nick Russo <nijaru0x@gmail.com>
2021-01-11 13:30:51 -0800

    add gitconfig to install
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -svf $HOME/github/dotfiles/.zprofile $HOME/.zprofile
 ln -svf $HOME/github/dotfiles/.zshrc $HOME/.zshrc
 ln -svf $HOME/github/dotfiles/.linuxrc $HOME/.linuxrc
 ln -svf $HOME/github/dotfiles/.macrc $HOME/.macrc
+ln -svf $HOME/github/dotfiles/.gitconfig $HOME/.gitconfig
 ln -svf $HOME/github/dotfiles/vimrc $HOME/.vim/vimrc
 ln -svf $HOME/github/dotfiles/init.toml $HOME/.Spacevim.d/init.toml
 

8c36672595ad6503f3716f2afc451ec82586c8a6
Nick Russo <nijaru0x@gmail.com>
2021-06-29 02:45:59 -0700

    install: update and fmt
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# dotfiles
 ln -svf $HOME/github/dotfiles/.aliases $HOME/.aliases
 ln -svf $HOME/github/dotfiles/.functions $HOME/.functions
 ln -svf $HOME/github/dotfiles/.p10k.zsh $HOME/.p10k.zsh
@@ -9,8 +10,12 @@ ln -svf $HOME/github/dotfiles/.zshrc $HOME/.zshrc
 ln -svf $HOME/github/dotfiles/.linux $HOME/.linux
 ln -svf $HOME/github/dotfiles/.macos $HOME/.macos
 ln -svf $HOME/github/dotfiles/.gitconfig $HOME/.gitconfig
-ln -svf $HOME/github/dotfiles/vimrc $HOME/.vim/vimrc
-ln -svf $HOME/github/dotfiles/init.toml $HOME/.Spacevim.d/init.toml
+# config
+ln -svf $HOME/github/dotfiles/htoprc $HOME/.config/htop/htoprc
+# vim
+# ln -svf $HOME/github/dotfiles/vimrc $HOME/.vim/vimrc
+# ln -svf $HOME/github/dotfiles/init.toml $HOME/.Spacevim.d/init.toml
+# emacs
 ln -svf $HOME/github/dotfiles/.doom.d/config.el $HOME/.doom.d/config.el
 ln -svf $HOME/github/dotfiles/.doom.d/init.el $HOME/.doom.d/init.el
 ln -svf $HOME/github/dotfiles/.doom.d/packages.el $HOME/.doom.d/packages.el

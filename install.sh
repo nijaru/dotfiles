cb11b0ab9790c5ab691d5b946551b6ab39a28b71
Nick Russo <nijaru0x@gmail.com>
2022-07-17 06:34:58 -0700

    install: rm editors
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- install.sh
+++ install.sh
@@ -10,14 +10,6 @@ ln -svf $HOME/github/dotfiles/.zshrc $HOME/.zshrc
 ln -svf $HOME/github/dotfiles/.linux $HOME/.linux
 ln -svf $HOME/github/dotfiles/.macos $HOME/.macos
 ln -svf $HOME/github/dotfiles/.gitconfig $HOME/.gitconfig
-# config
 ln -svf $HOME/github/dotfiles/htoprc $HOME/.config/htop/htoprc
-# vim
-# ln -svf $HOME/github/dotfiles/vimrc $HOME/.vim/vimrc
-# ln -svf $HOME/github/dotfiles/init.toml $HOME/.Spacevim.d/init.toml
-# emacs
-ln -svf $HOME/github/dotfiles/.doom.d/config.el $HOME/.doom.d/config.el
-ln -svf $HOME/github/dotfiles/.doom.d/init.el $HOME/.doom.d/init.el
-ln -svf $HOME/github/dotfiles/.doom.d/packages.el $HOME/.doom.d/packages.el
 
 echo "Install complete."

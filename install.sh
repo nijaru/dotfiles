e5e8fd9e3b3576e1a585d9149cba7c276f9aacf7
Nick Russo <nijaru0x@gmail.com>
2024-02-18 15:57:55 -0800

    Move home/ to .

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env zsh
 
 # dotfiles
-ln -svf $HOME/github/dotfiles/home/.zshrc $HOME/.zshrc
-ln -svf $HOME/github/dotfiles/home/.zshenv $HOME/.zshenv
-ln -svf $HOME/github/dotfiles/home/.aliases $HOME/.aliases
-ln -svf $HOME/github/dotfiles/home/.p10k.zsh $HOME/.p10k.zsh
+ln -svf $HOME/github/dotfiles/.zshrc $HOME/.zshrc
+ln -svf $HOME/github/dotfiles/.zshenv $HOME/.zshenv
+ln -svf $HOME/github/dotfiles/.aliases $HOME/.aliases
+ln -svf $HOME/github/dotfiles/.p10k.zsh $HOME/.p10k.zsh
 
 # git
-ln -svf $HOME/github/dotfiles/home/.gitconfig $HOME/.gitconfig
+ln -svf $HOME/github/dotfiles/.gitconfig $HOME/.gitconfig
 
 # ssh
 mkdir -p $HOME/.ssh
-ln -svf $HOME/github/dotfiles/home/.ssh/config $HOME/.ssh/config
+ln -svf $HOME/github/dotfiles/.ssh/config $HOME/.ssh/config
 
 # OS specific
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ln -svf $HOME/github/dotfiles/home/.linux $HOME/.linux
+    ln -svf $HOME/github/dotfiles/.linux $HOME/.linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    ln -svf $HOME/github/dotfiles/home/.macos $HOME/.macos
+    ln -svf $HOME/github/dotfiles/.macos $HOME/.macos
     mkdir -p $HOME/.gnupg
-    ln -svf $HOME/github/dotfiles/home/.gnupg/gpg.conf $HOME/.gnupg/gpg.conf
+    ln -svf $HOME/github/dotfiles/.gnupg/gpg.conf $HOME/.gnupg/gpg.conf
 fi
 
 echo "Install complete."

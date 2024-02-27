9d2e7755965cb736c2cd48d5b380ff566ef89462
Nick Russo <nijaru0x@gmail.com>
2024-02-26 17:26:47 -0800

    Add zed settings

--- install.sh
+++ install.sh
@@ -15,11 +15,17 @@ ln -svf $HOME/github/dotfiles/.ssh/config $HOME/.ssh/config
 
 # OS specific
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # linux
     ln -svf $HOME/github/dotfiles/.linux $HOME/.linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macos
     ln -svf $HOME/github/dotfiles/.macos $HOME/.macos
+    # gpg
     mkdir -p $HOME/.gnupg
     ln -svf $HOME/github/dotfiles/.gnupg/gpg.conf $HOME/.gnupg/gpg.conf
+    # zed
+    mkdir -p $HOME/.config/zed
+    ln -svf $HOME/github/dotfiles/zed/settings.json $HOME/.config/zed/settings.json
 fi
 
 echo "Install complete."

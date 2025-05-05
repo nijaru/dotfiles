c6439d9a2ad2dae07c505474252be536c214819d
Nick Russo <nijaru7@gmail.com>
2025-05-05 06:18:17 -0700

    Add orbstack to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -66,3 +66,6 @@ set -g fish_key_bindings fish_default_key_bindings  # Default key bindings
 if status is-interactive; and type -q starship
     starship init fish | source
 end
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :

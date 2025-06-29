48dc7d6d96fdacb6f662ecfb51577ebcb8ae7efe
Nick Russo <nijaru7@gmail.com>
2025-06-29 04:23:42 -0700

    Add Git submodule recursion abbreviation

--- dot_config/fish/conf.d/git_abbr.fish
+++ dot_config/fish/conf.d/git_abbr.fish
@@ -5,7 +5,7 @@ if status is-interactive
     # Core commands
     abbr --add g git
     abbr --add gi 'git init'
-    
+
     # Status and Information
     abbr --add gs 'git status'
     abbr --add gd 'git diff --color-words'
@@ -13,7 +13,7 @@ if status is-interactive
     abbr --add gds 'git diff --staged'
     abbr --add gdc 'git diff --cached'
     abbr --add gwt 'git worktree'
-    
+
     # Branch Management
     abbr --add gb 'git branch -v'
     abbr --add gbv 'git branch -vv'
@@ -23,14 +23,14 @@ if status is-interactive
     abbr --add gbd 'git branch -d'
     abbr --add gbD 'git branch -D'
     abbr --add gbc 'git branch --show-current'
-    
+
     # Branch switching
     abbr --add gsw 'git switch'
     abbr --add gswc 'git switch -c'
     abbr --add gswm 'git switch main'
     abbr --add gswd 'git switch dev'
     abbr --add gswb 'git switch -'
-    
+
     # Staging
     abbr --add ga 'git add'
     abbr --add ga. 'git add .'
@@ -39,14 +39,14 @@ if status is-interactive
     abbr --add gau 'git add --update'
     abbr --add grm 'git rm'
     abbr --add grmc 'git rm --cached'
-    
+
     # Unstaging
     abbr --add grs 'git restore'
     abbr --add grss 'git restore --staged'
     abbr --add grh1 'git reset HEAD~1'
     abbr --add gclean 'git clean -df'
     abbr --add gnuke 'git clean -dffx'
-    
+
     # Commits
     abbr --add gc 'git commit --gpg-sign'
     abbr --add gcm --set-cursor 'git commit --gpg-sign -m "%"'
@@ -55,7 +55,7 @@ if status is-interactive
     abbr --add gcf 'git commit --gpg-sign --amend --reuse-message HEAD'
     abbr --add gcF 'git commit --gpg-sign --amend'
     abbr --add gfix 'git commit --gpg-sign --fixup'
-    
+
     # Stash
     abbr --add gst 'git stash'
     abbr --add gstp 'git stash pop'
@@ -64,7 +64,7 @@ if status is-interactive
     abbr --add gstu 'git stash --include-untracked'
     abbr --add gstd 'git stash drop'
     abbr --add gstc 'git stash clear'
-    
+
     # Remote operations
     abbr --add gf 'git fetch --all --prune'
     abbr --add gpl 'git pull --rebase'
@@ -74,7 +74,7 @@ if status is-interactive
     abbr --add gpu 'git push -u'
     abbr --add gpuo 'git push -u origin'
     abbr --add gtrack 'git branch --set-upstream-to'
-    
+
     # Logging
     abbr --add gl 'git log --pretty=format:"%C(green)%h%C(auto)%d %s %C(cyan)%cr %C(blue)<%an>%C(reset)" -n 10'
     abbr --add gln 'git log --pretty=format:"%C(green)%h%C(auto)%d %s %C(cyan)%cr %C(blue)<%an>%C(reset)" -n'
@@ -85,7 +85,7 @@ if status is-interactive
     abbr --add glf 'git log --follow -p'
     abbr --add glast 'git log -1 HEAD --stat'
     abbr --add gwho 'git shortlog -s --no-merges'
-    
+
     # Merge
     abbr --add gm 'git merge'
     abbr --add gmnff 'git merge --no-ff'
@@ -93,17 +93,18 @@ if status is-interactive
     abbr --add gmc 'git merge --continue'
     abbr --add gms 'git merge --skip'
     abbr --add gma 'git merge --abort'
-    
+
     # Rebase
     abbr --add gr 'git rebase'
     abbr --add gri 'git rebase -i'
     abbr --add grc 'git rebase --continue'
     abbr --add gra 'git rebase --abort'
     abbr --add grm 'git rebase main'
-    
+
     # Maintenance
     abbr --add greflog 'git reflog'
     abbr --add gverify 'git verify-commit HEAD'
     abbr --add gcleanup 'git clean -xfd'
     abbr --add ggarbage 'git gc --aggressive --prune=now'
+    abbr --add gsubr 'git config submodule.recurse true'
 end

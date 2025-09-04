8688147752554444c5ef767cad1eaa77795ddfe1
Nick Russo <nijaru7@gmail.com>
2025-09-04 03:45:01 -0700

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -96,3 +96,5 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# set -gx ANTHROPIC_API_KEY ""

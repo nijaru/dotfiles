64e50653356806f26fa782e282d54444dc21cfcf
Nick Russo <nijaru7@gmail.com>
2025-10-22 13:53:38 -0700

    refactor: prune unused Fish functions and enhance portable config
    Remove unused functions:
    - navigation/d (duplicate of dl)
    - editor shortcuts: z, zp, zp_dot (z conflicts with zoxide, others unused)
    - kubernetes: all 4 functions (kctx, kns, kpods, klogs - not currently using k8s)
    - utils: genpass, randstr, uuid, edit_with_mkdir (unused)
    
    Enhance portable config:
    - Add extract (archive extraction)
    - Add jsonf (JSON formatting)
    - Add timecmd (profile command execution)
    - Add navigation shortcuts (dl, dt, doc, ghub)
    
    Kept functions reflect actual usage patterns from shell history

--- dot_config/fish/functions/editor/zp.fish
+++
@@ -1,3 +0,0 @@
-function zp --description 'Open in zed-preview with directory creation'
-    edit_with_mkdir zed-preview $argv
-end
--- dot_config/fish/functions/kubernetes/klogs.fish
+++
@@ -1,4 +0,0 @@
-function klogs --description 'View Kubernetes pod logs'
-    set -l POD (kubectl get pods | grep "$argv[1]" | awk '{print $1}')
-    kubectl logs -f "$POD" $argv[2..-1]
-end

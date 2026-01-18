00ac0b670cca23bca7786f70cbe04fc3d4bdbe5b
Nick Russo <nijaru7@gmail.com>
2026-01-18 13:46:44 -0800

    Sync fish (LM Studio CLI) and ghostty (auto-update tip) from mac

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -85,3 +85,8 @@ end
 
 # OrbStack CLI integration
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/nick/.lmstudio/bin
+# End of LM Studio CLI section
+
